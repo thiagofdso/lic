1d06a046e04df605d667e18063a566a5d018b60b
Thiago Oliveira <thiagofdso.ufpa@gmail.com>
2016-07-11 16:38:03 -0300

    adicionado aliases no app.config e midleware no kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \LucaDegasperi\OAuth2Server\Middleware\OAuthExceptionHandlerMiddleware::class,
     ];
 
     /**
