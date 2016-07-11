6894877b102b30129a5b6666abf1b0fb51481dc5
Thiago Oliveira <thiagofdso.ufpa@gmail.com>
2016-07-11 18:56:39 -0300

    adcionados grupo de usuários client e deliveryman

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -61,16 +61,20 @@ Route::post('oauth/access_token', function() {
 
 Route::group(['prefix'=>'api','middleware'=>'oauth','as'=>'api.'],function() {
     Route::group(['prefix'=>'client','as'=>'client.'],function() {
-
+        Route::get('pedidos',function (){
+            return [
+                'id'=>'1',
+                'client'=>'Thiago Oliveira',
+                'total'=>10];
+        });
     });
-    Route::group(['prefix'=>'client','as'=>'client.'],function() {
-
-    });
-    Route::get('pedidos',function (){
-        return [
-        'id'=>'1',
-        'client'=>'Thiago Oliveira',
-        'total'=>10];
+    Route::group(['prefix'=>'deliveryman','as'=>'deliveryman.'],function() {
+        Route::get('pedidos',function (){
+            return [
+                'id'=>'1',
+                'client'=>'Thiago Fernandes',
+                'total'=>15];
+        });
     });
 
 });
